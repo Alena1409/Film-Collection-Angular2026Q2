68a2c6e177798543fae86cf15d128cac0f9bed75
Alena Alekseeva <a77993519@gmail.com>
2026-04-28 15:49:36 +0400

    feat: add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/catalog/catalog').then((m) => m.Catalog),
+  },
+  {
+    path: 'film',
+    loadComponent: () => import('./pages/film/film').then((m) => m.Film),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about').then((m) => m.About),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/not-found/not-found').then((m) => m.NotFound),
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Breadcrumbs } from "./core/layout/breadcrumbs/breadcrumbs";
+import { Header } from "./core/layout/header/header";
+import { Footer } from "./core/layout/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Breadcrumbs, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
