93e8dfc46f5552365c810b74f0bf123b7a6e5e36
Alena Alekseeva <a77993519@gmail.com>
2026-04-29 23:45:04 +0400

    feat: add header

--- src/app/core/layout/header/header.ts
+++ src/app/core/layout/header/header.ts
@@ -1,11 +1,14 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, DatePipe],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
-
+  protected readonly currentDate = new Date();
+  protected readonly filmsCount = 21;
 }
